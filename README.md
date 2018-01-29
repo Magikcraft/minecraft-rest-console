28844794c0e5ee0cbda797f179a90b5d01a549ff
Joshua Wulf <sitapati@worldsankirtan.net>
2018-01-29 19:52:42 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Magikcraft Rest Console
+
+This Minecraft plugin provides a REST server that allows you to issue commands via REST to execute as the server console or as any online player.
+
+## Configuration
+The port is configurable via the environment. Set the environment variable `MAGIKCRAFT_HTTPD_PORT` to the port you want the server to run on.
+
+The default is port 8086 if no port is set.
+
+The endpoint can be secured with an API key. Set the environment variable `MAGIKCRAFT_HTTPD_API_KEY` to the API key you with to use to secure the endpoint.
+
+The default is to run the endpoint unsecured.
+
+## Usage
+
+### echo
+
+The echo route provides a simple method to test that the endpoint is operating.
+
+It will respond with the message that you send, and also log the message out to the server console / server log.
+
+The following route will work with the default configuration (unsecured, port 8086) endpoint:
+
+```bash
+http://localhost:8086/echo?message=this%20is%20the%20message
+```
+
+With an API key, the request would look like this:
+
+```bash
+http://localhost:8086/echo?message=this%20is%20the%20message&apikey=MySuperSecretKey1001
+```
+
+### remoteExecuteCommand
+
+Assuming that the endpoint is running on the default port of 8086 and unsecured (by default), the following command will execute `js refresh()` as the server console:
+
+```bash
+http://localhost:8086/remoteExecuteCommand?player=server&command=js%20refresh()
+```
+
+With an API key set, the request would look like:
+
+```bash
+http://localhost:8086/remoteExecuteCommand?player=server&command=js%20refresh()&apikey=SomeSecretKey
+```
+
+### sendMessageToPlayer
+
+You can send a message to an online player:
+
+```bash
+http://localhost:8086/sendMessageToPlayer?player=sitapati&message=this%20is%20the%20message
+```
+
+With an API key:
+
+
+```bash
+http://localhost:8086/sendMessageToPlayer?player=sitapati&message=this%20is%20the%20message&apikey=13242345jkldsf*
+```
+
+## Responses
+
+When the endpoint is secured, if you are not authorised with the correct API Key, you will get a 403 Forbidden response.
+
+If the endpoint is not secured, or you have the correct API key, the response is always a HTTP Status code 200.
+
+It will be a JSON object with an `ok` field. This will be `true` if the request processed ok, and `false` if it did not. 
+
+For example, if you try to send a message to a player who is not online, you will get a `{ok: false}` response.
---
+++ src/io/magikcraft/rest/MagikcraftRest.kt
@@ -0,0 +1,180 @@
+package io.magikcraft.rest
+
+import org.apache.commons.lang.StringUtils
+import org.bukkit.plugin.java.JavaPlugin
+import org.json.simple.JSONObject
+import java.net.URLDecoder
+import java.util.logging.Level
+
+@Suppress("unused")
+class MagikcraftRest() : JavaPlugin() {
+    private lateinit var httpd: Httpd
+
+    /**
+     * This is a singleton, but we have to use a Kotlin class because it is managed by Bukkit.
+     * So we use a companion object to enforce the singleton pattern.
+     */
+    companion object {
+        var instance: MagikcraftRest? = null
+            private set
+    }
+
+    override fun onEnable() {
+        instance = this
+        val key = System.getenv("MAGIKCRAFT_HTTPD_API_KEY")
+        val configuredPort = System.getenv("MAGIKCRAFT_HTTPD_PORT")
+        var port: Int
+        try {
+            if (configuredPort != null) port = configuredPort.toInt()
+            else port = 8086
+        } catch (e: Exception) {
+            port = 8086
+        }
+        httpd = Httpd(this, port, key)
+        httpd.start()
+        logger.info("Magikcraft REST Console plugin enabled!")
+        logger.info("Magikcraft REST Console started on Port " + port)
+        if (key == null)
+            logger.info("REST endpoint unsecured")
+        else
+            logger.info("REST endpoint secured with key")
+    }
+
+    fun sendMessageTo(playerName: String, msg: String): Boolean {
+        if (server.getPlayer(playerName) != null) {
+            server.getPlayer(playerName).sendMessage(msg)
+            return true
+        }
+        return false
+    }
+
+    fun broadcastMessage(msg: String) {
+        server.broadcastMessage(msg)
+    }
+
+    private class Httpd(plugin: MagikcraftRest, port: Int, key: String?) : NanoHTTPD(port) {
+
+        private val plugin = plugin
+        private val logger = plugin.logger
+        private val port = port
+        private val API_KEY = key
+
+        private fun isAuthorised(session: IHTTPSession): Boolean {
+            return (API_KEY == null || API_KEY == getParameter("apikey", session, ""))
+        }
+
+        private fun getParameter(key: String, session: IHTTPSession): String {
+            return getParameter(key, session, "")
+        }
+
+        private fun getParameter(key: String, session: IHTTPSession, defaultValue: String): String {
+            val params = session.parms
+            if (!params.isEmpty()) {
+                if (params.containsKey(key)) {
+                    val proxyRequestParam = params[key]
+                    try {
+                        return URLDecoder.decode(proxyRequestParam?.replace("+", "%2B"), "UTF-8")
+                                .replace("%2B", "+")
+                    } catch (ex: Throwable) {
+                        this.logger.log(Level.WARNING, ex.toString())
+                    }
+
+                }
+            }
+            return defaultValue
+        }
+
+        private fun OK(msg: String): NanoHTTPD.Response {
+            return newFixedLengthResponse(Response.Status.OK,
+                    "application/json",
+                    "{\"ok\":\"true\", \"msg\":\"$msg\"}")
+        }
+
+        private fun OK(json: JSONObject): NanoHTTPD.Response {
+            return newFixedLengthResponse(Response.Status.OK,
+                    "application/json",
+                    json.toJSONString())
+        }
+
+        private fun notOK(msg: String): NanoHTTPD.Response {
+            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR,
+                    "application/json",
+                    "{\"ok\":\"false\", \"msg\":\"$msg\"}")
+        }
+
+        private fun _404(): NanoHTTPD.Response {
+            return newFixedLengthResponse(Response.Status.NOT_FOUND,
+                    NanoHTTPD.MIME_PLAINTEXT,
+                    "Error 404: Not Found - The requested resource was not found on this server.")
+        }
+
+        private fun _403(): NanoHTTPD.Response {
+            logger.log(Level.INFO, "Request not authorised - 403")
+            return newFixedLengthResponse(Response.Status.FORBIDDEN,
+                    NanoHTTPD.MIME_PLAINTEXT,
+                    "Error 403: Forbidden - You do not have permission to access the requested resource.")
+        }
+
+        private fun messageDataToPlayer(session: IHTTPSession, playerName: String) {
+            val msg = getParameter("data", session)
+            plugin.sendMessageTo(playerName, msg)
+        }
+
+        private fun messageDataToPlayers(session: IHTTPSession) {
+            val msg = getParameter("data", session)
+            println("Message to all players: " + msg)
+            plugin.broadcastMessage(msg)
+        }
+
+        override fun serve(session: IHTTPSession): NanoHTTPD.Response {
+            val args = StringUtils.split(session.uri, "/")
+            if (args.size > 0) {
+                val route = args[0]
+                when (route) {
+                    "remoteExecuteCommand" -> return remoteExecuteCommand(session, args)
+                    "sendMessageToPlayer" -> return sendMessageToPlayer(session, args)
+                    "echo" -> return echo(session, args)
+                }
+            }
+            return _404()
+        }
+
+        private fun echo(session: IHTTPSession, args: Array<String>): NanoHTTPD.Response {
+            val message = getParameter("message", session)
+            logger.info("Echo: $message")
+            return OK(message)
+        }
+
+        private fun remoteExecuteCommand(session: IHTTPSession, args: Array<String>): NanoHTTPD.Response {
+            if (!isAuthorised(session)) {
+                return _403()
+            }
+            val playerName = getParameter("player", session, "server")
+            val command = getParameter("command", session)
+            if ("server".equals(playerName, ignoreCase = true)) { // Run as server
+                val sender = plugin.getServer().getConsoleSender()
+                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, { plugin.getServer().dispatchCommand(sender, command) }, 1)
+                return OK("Command scheduled for execution")
+
+            } else {
+                val sender = plugin.getServer().getPlayer(playerName)
+                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, { plugin.getServer().dispatchCommand(sender, command) }, 1)
+                return OK("Command returned true")
+            }
+        }
+
+        private fun sendMessageToPlayer(session: IHTTPSession, args: Array<String>): NanoHTTPD.Response {
+            val playerName = getParameter("player", session)
+            val msg = getParameter("msg", session)
+            val success = plugin.sendMessageTo(playerName, msg)
+            return if (success) {
+                logger.log(Level.INFO, "Sent to player $playerName: $msg")
+                OK("Message sent")
+            } else {
+                logger.log(Level.INFO, "Send message $msg to player $playerName failed. Player not found.")
+                notOK("Sending message failed - Player $playerName not found.")
+            }
+        }
+
+    }
+}
